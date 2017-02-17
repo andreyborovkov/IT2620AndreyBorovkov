00eb233d855f68bad99455ab753ab0a5804caf33
Andrey Borovkov <s01068439@acad.tri-c.edu>
2017-02-17 16:56:11 -0500

    Created Lab05 Project part 1

---
+++ Lab05-LargerSmaller/Lab05-LargerSmaller/Form1.Designer.vb
@@ -0,0 +1,134 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.txtA = New System.Windows.Forms.TextBox()
+        Me.txtB = New System.Windows.Forms.TextBox()
+        Me.lblA = New System.Windows.Forms.Label()
+        Me.lblB = New System.Windows.Forms.Label()
+        Me.lblInstructions = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.btnCompare = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtA
+        '
+        Me.txtA.Location = New System.Drawing.Point(108, 62)
+        Me.txtA.Name = "txtA"
+        Me.txtA.Size = New System.Drawing.Size(100, 22)
+        Me.txtA.TabIndex = 0
+        '
+        'txtB
+        '
+        Me.txtB.Location = New System.Drawing.Point(281, 62)
+        Me.txtB.Name = "txtB"
+        Me.txtB.Size = New System.Drawing.Size(100, 22)
+        Me.txtB.TabIndex = 1
+        '
+        'lblA
+        '
+        Me.lblA.AutoSize = True
+        Me.lblA.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblA.Location = New System.Drawing.Point(81, 65)
+        Me.lblA.Name = "lblA"
+        Me.lblA.Size = New System.Drawing.Size(23, 17)
+        Me.lblA.TabIndex = 2
+        Me.lblA.Text = "A:"
+        '
+        'lblB
+        '
+        Me.lblB.AutoSize = True
+        Me.lblB.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblB.Location = New System.Drawing.Point(254, 65)
+        Me.lblB.Name = "lblB"
+        Me.lblB.Size = New System.Drawing.Size(23, 17)
+        Me.lblB.TabIndex = 3
+        Me.lblB.Text = "B:"
+        '
+        'lblInstructions
+        '
+        Me.lblInstructions.AutoSize = True
+        Me.lblInstructions.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblInstructions.Location = New System.Drawing.Point(143, 27)
+        Me.lblInstructions.Name = "lblInstructions"
+        Me.lblInstructions.Size = New System.Drawing.Size(168, 20)
+        Me.lblInstructions.TabIndex = 4
+        Me.lblInstructions.Text = "Enter Two Integers"
+        '
+        'lblResult
+        '
+        Me.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblResult.Location = New System.Drawing.Point(33, 114)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(389, 35)
+        Me.lblResult.TabIndex = 5
+        Me.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnCompare
+        '
+        Me.btnCompare.Location = New System.Drawing.Point(103, 185)
+        Me.btnCompare.Name = "btnCompare"
+        Me.btnCompare.Size = New System.Drawing.Size(105, 45)
+        Me.btnCompare.TabIndex = 6
+        Me.btnCompare.Text = "Compare"
+        Me.btnCompare.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(245, 185)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(105, 45)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(460, 267)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCompare)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.lblInstructions)
+        Me.Controls.Add(Me.lblB)
+        Me.Controls.Add(Me.lblA)
+        Me.Controls.Add(Me.txtB)
+        Me.Controls.Add(Me.txtA)
+        Me.Name = "Form1"
+        Me.Text = "Larger and Smaller"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtA As TextBox
+    Friend WithEvents txtB As TextBox
+    Friend WithEvents lblA As Label
+    Friend WithEvents lblB As Label
+    Friend WithEvents lblInstructions As Label
+    Friend WithEvents lblResult As Label
+    Friend WithEvents btnCompare As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Lab05-LargerSmaller/Lab05-LargerSmaller/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+    Private Sub btnCompare_Click(sender As Object, e As EventArgs) Handles btnCompare.Click
+
+        'Declaring variables
+        Dim intA As Integer
+        Dim intB As Integer
+
+        'Checking whether the variables in the text boxes are integers and assigning them to the variables if they are
+        If Integer.TryParse(txtA.Text, intA) And Integer.TryParse(txtB.Text, intB) Then
+            Try
+
+                If intA > intB Then
+                    lblResult.Text = "Values A is greatest"
+                ElseIf intA < intB Then
+                    lblResult.Text = "Value B is greatest"
+                Else
+                    lblResult.Text = "Values are equal"
+                End If
+
+            Catch
+                lblResult.Text = "Something went wrong"
+            End Try
+
+
+        Else
+            lblResult.Text = "Invalid Data" 'Displays the message if any of the variables is not an integer
+        End If
+
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
