72b96fb3569548cb6c6eb5e5dfca668fd5b0c9e8
Andrey Borovkov <s01068439@acad.tri-c.edu>
2017-02-26 14:41:11 -0500

    Created Lab06 project Part 2

---
+++ Lab06DistanceCalculator/Lab06DistanceCalculator/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstOutput = New System.Windows.Forms.ListBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstOutput
+        '
+        Me.lstOutput.FormattingEnabled = True
+        Me.lstOutput.ItemHeight = 16
+        Me.lstOutput.Location = New System.Drawing.Point(47, 36)
+        Me.lstOutput.Name = "lstOutput"
+        Me.lstOutput.Size = New System.Drawing.Size(445, 340)
+        Me.lstOutput.TabIndex = 0
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(125, 407)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(121, 55)
+        Me.btnCalculate.TabIndex = 1
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(286, 407)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(121, 55)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(581, 520)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lstOutput)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lstOutput As ListBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Lab06DistanceCalculator/Lab06DistanceCalculator/Form1.vb
@@ -0,0 +1,73 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim intCount As Integer
+        Dim intDistance As Integer
+        Dim intTotal As Integer
+        Dim strTemp As String
+        'For some reasons, although vbTab command works in my string objects, commands vbNewLine and vbCrLf do not, 
+        'so I had to create objects for each line of my output description. 
+        Dim strSpeedDescription As String
+        Dim strTimeDescription As String
+        Dim strColumns As String
+        Dim strBreak As String
+
+        Dim strTotal As String
+
+        Try
+            'Variables for Speed Input box
+            Dim strSpeedInput As String
+            Dim intSpeed As Integer
+            strSpeedInput = InputBox("Enter the speed of the vehicle in miles per hour.", "Input Needed")
+            intSpeed = CInt(strSpeedInput)
+
+            'Variables for Time Input box
+            Dim strTimeInput As String
+            Dim intTime As Integer
+            strTimeInput = InputBox("Enter the amount of time, in hours, that the vehicle has traveled", "Input Needed")
+            intTime = CInt(strTimeInput)
+
+            If Integer.TryParse(strSpeedInput, intSpeed) And intSpeed > 0 And Integer.TryParse(strTimeInput, intTime) And intTime > 0 Then
+
+
+                'Display the description
+                strSpeedDescription = "Vehicle Speed: " & intSpeed.ToString() & " MPH"
+                strTimeDescription = "Time Traveled: " & intTime.ToString() & vbCrLf & " hours"
+                strColumns = "Hours" & vbTab & "Distance Traveled"
+                strBreak = "--------------------------------------------"
+
+                lstOutput.Items.Add(strSpeedDescription)
+                lstOutput.Items.Add(strTimeDescription)
+                lstOutput.Items.Add(strColumns)
+                lstOutput.Items.Add(strBreak)
+
+                For intCount = 1 To intTime
+                    intDistance = CInt(intCount * intSpeed)
+                    intTotal += intDistance
+
+                    'String to display
+                    strTemp = intCount.ToString() & vbTab & intDistance.ToString()
+                    lstOutput.Items.Add(strTemp)
+
+                Next
+
+                'Display Total
+
+                strTotal = "Total Distance: " & intTotal.ToString()
+                lstOutput.Items.Add(strTotal)
+
+            Else
+                MessageBox.Show("Invalid Data")
+            End If
+
+
+        Catch
+            MessageBox.Show("Something went wrong")
+
+        End Try
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
